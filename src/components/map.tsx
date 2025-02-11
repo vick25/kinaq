d5ffa043db2856bfaba9b45ccfc104f3e4271c8d
Victor Kadiata <vickadiata@gmail.com>
2025-02-11 19:24:12 +0100

    Refactoring the Map component

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,51 +1,55 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react';
-import maplibregl, { GeolocateControl, NavigationControl, AttributionControl, ScaleControl } from "maplibre-gl";
+import maplibregl, { Map, GeolocateControl, NavigationControl, AttributionControl, ScaleControl } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import useLocationStore from '../../stores/location-store';
 import { IMapComponentProps } from '@/lib/definitions';
 import { getPM25Color } from '@/lib/utils';
 
 const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
+    const [map, setMap] = useState<Map | null>(null);
     const [message, setMessage] = useState('')
     const [locationId, setLocationId] = useState<number | null>(-1)
     const { retrieveLocation } = useLocationStore();
 
-    const mapContainer = useRef<HTMLDivElement>(null);
-    const mapRef = useRef<maplibregl.Map | null>(null);
-
+    // To Load the Map
     useEffect(() => {
-        if (mapRef.current) return; // Prevent reinitialization
+        if (map) return;
 
         const initialLongitude = 20.0; // Longitude for the center of Central Africa
         const initialLatitude = 0.0;   // Latitude for the center of Central Africa
         const initialZoom = 2;         // Adjust zoom level as needed
         const initialBounds: [number, number, number, number] = [-10, -15, 45, 15];
+        // This is used to initialize the map. The library used here is Maplibre GL JS (https://maplibre.org).
+        setMap(
+            new maplibregl.Map({
+                container: 'map',
+                style:
+                    // 'https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json',
+                    "https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+                center: [initialLongitude, initialLatitude],
+                zoom: initialZoom,
+                // maxBounds: initialBounds,
+                attributionControl: false
+            })
+                .addControl(new NavigationControl(), 'top-left')
+                .addControl(new GeolocateControl({
+                    positionOptions: { enableHighAccuracy: true },
+                }))
+                .addControl(new ScaleControl({
+                    maxWidth: 80,
+                    unit: 'metric'
+                }))
+                .addControl(new AttributionControl({
+                    customAttribution: '<a href="https://www.airgradient.com/" target="_blank">&copy; AirGradient</a> | <a href="https://www.wasaru.org/" target="_blank">&copy; WASARU </a>',
+                    compact: true,
+                }))
+        );
+    }, [map]);
 
-        const map = new maplibregl.Map({
-            container: mapContainer.current as HTMLDivElement,
-            style:
-                // 'https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json',
-                "https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
-            center: [initialLongitude, initialLatitude],
-            zoom: initialZoom,
-            // maxBounds: initialBounds,
-            attributionControl: false
-        });
-
-        map.addControl(new NavigationControl(), 'top-left');
-        map.addControl(new GeolocateControl({
-            positionOptions: { enableHighAccuracy: true },
-        }));
-        map.addControl(new ScaleControl({
-            maxWidth: 80,
-            unit: 'metric'
-        }));
-        map.addControl(new AttributionControl({
-            customAttribution: '<a href="https://www.airgradient.com/" target="_blank">&copy; AirGradient</a> | <a href="https://www.wasaru.org/" target="_blank">&copy; WASARU </a>',
-            compact: true,
-        }));
+    useEffect(() => {
+        if (!map) return; // Prevent reinitialization
 
         map.on('wheel', (e) => {
             if (!e.originalEvent.ctrlKey) {
@@ -61,10 +65,8 @@ const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
             }
         });
 
-        mapRef.current = map;
-
         // Fetch and add points to the map
-        const fetchData = async () => {
+        const populateMarkers = async () => {
             try {
                 const pointsGeoJSON: any = {
                     type: "FeatureCollection",
@@ -95,6 +97,8 @@ const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
                         source: "locations",
                         paint: {
                             "circle-radius": 8,
+                            "circle-stroke-width": 1,
+                            "circle-stroke-color": "#fff",
                             // "circle-color": "#FF5722",
                             'circle-color': ['step',
                                 ['get', 'pm2_5'],
@@ -113,7 +117,7 @@ const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
                         },
                     });
 
-                    map.on("click", "location-points", (e) => {
+                    map.on("click", "location-points", async (e: any) => {
                         const coordinates = e.features?.[0].geometry.coordinates.slice();
                         const { locationId, locationName, pm2_5, timestamp } = e.features?.[0].properties || {};
 
@@ -121,17 +125,17 @@ const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
                             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                         }
 
-                        retrieveLocation(locationId); // Store the locationId
-                        setLocationId(locationId);
-
                         new maplibregl.Popup()
                             .setLngLat(coordinates as [number, number])
                             .setHTML(
                                 `<strong>${locationName}</strong><br>
-                                PM2.5: ${pm2_5}<br>
-                                Time: ${new Date(timestamp).toLocaleString()}`
+                            PM2.5: ${pm2_5}<br>
+                            Time: ${new Date(timestamp).toLocaleString()}`
                             )
                             .addTo(map);
+
+                        retrieveLocation(locationId); // Store the locationId
+                        setLocationId(locationId);
                     });
 
                     map.on("mouseenter", "location-points", () => {
@@ -147,19 +151,17 @@ const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
             }
         };
 
-        fetchData();
+        populateMarkers();
+    }, [map]);
 
-        return () => {
-            map.remove();
-        };
-    }, []);
-
+    // Use the user last location as the default air quality information when the application loads
     useEffect(() => {
+        if (!map) return;
         // Fly to a random location by offsetting the point -74.50, 40
         // by up to 5 degrees.
         locationId &&
             setTimeout(() => {
-                mapRef.current?.flyTo({
+                map.flyTo({
                     center: [
                         -74.5 + (Math.random() - 0.5) * 10,
                         40 + (Math.random() - 0.5) * 10
@@ -167,16 +169,16 @@ const MapComponent: React.FC<IMapComponentProps> = ({ gradientData }) => {
                     essential: true // this animation is considered essential with respect to prefers-reduced-motion
                 })
             }, 5000);
-    }, [locationId])
+    }, [map, locationId])
 
     return (
-        <div className='relative h-full w-full' style={{ position: 'relative', height: '100%', width: '100%' }}>
+        <div className='relative h-full w-full'>
             {message && (
                 <div className='absolute top-0 w-full h-full flex justify-center items-center bg-white opacity-70 text-gray-600 z-[2]'>
                     {message}
                 </div>
             )}
-            <div ref={mapContainer} style={{ height: '100%', width: '100%' }} />
+            <div id='map' className='h-full w-full' />
         </div>
     );
 };
