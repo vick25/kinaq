4e5562356980e735dead030cb75718503691bee2
Victor Kadiata <vickadiata@gmail.com>
2025-03-22 22:06:39 +0100

    Historical pages and Export data component added

---
+++ src/app/historical/page.tsx
@@ -0,0 +1,72 @@
+import ExportData from '@/components/export-data'
+import React from 'react'
+
+type Props = {}
+
+const Historical = (props: Props) => {
+    return (
+        <div className='container mx-auto'>
+            <section className='my-5 space-y-4'>
+                <div className="text-[#f0f0f0] bg-[#222] text-2xl font-semibold leading-5 relative pl-[2.3rem] pr-0 py-4 rounded-[3rem_0_0_3rem]">
+                    KINSHASA | Data Export
+                </div>
+
+                <div className='mt-8 w-full'>
+                    <div>
+                        <h2 className="text-xl font-semibold text-gray-800">
+                            Download active air quality information older than the last 7 days:
+                        </h2>
+                        <ul className="mt-2 space-y-1 list-inside list-disc relative left-8">
+                            <li className="text-blue-600 hover:underline">
+                                comma-separated text files (.csv) or
+                            </li>
+                            <li className="text-blue-600 hover:underline">
+                                JSON files (.json)
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="my-6 bg-white border shadow-md rounded-md p-6 max-w-3xl mx-auto">
+                        <div>
+                            <h3 className="text-lg font-semibold text-gray-800 text-center">
+                                Authentication through Email:
+                            </h3>
+                            {/* Earthdata Login Button */}
+                            {/* <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-md text-lg font-medium hover:bg-blue-700">
+                                Earthdata Login
+                            </button>
+                            <hr className="my-4 border-gray-300" /> */}
+                            {/* Email Input */}
+                            <input
+                                type="email"
+                                placeholder="Enter email address"
+                                className="w-full border p-2 rounded-md text-gray-700"
+                            />
+                            {/* Email Code Button */}
+                            <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-md text-lg font-medium hover:bg-blue-700">
+                                Email Code
+                            </button>
+                        </div>
+
+                        <div id="email_code_block">
+                            <h3 className="text-lg font-semibold text-gray-800 text-center">Enter CODE emailed to:</h3>
+                            <div className="lbCode">
+                                <input type="text" name="code" id="ecode" value="" placeholder="123456"
+                                    className="w-full border p-2 rounded-md text-gray-700" />
+                            </div>
+                            <p id="codeError" className='text-xs text-red-600'></p>
+                            <div className='flex space-x-4 items-center'>
+                                <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-md text-lg font-medium hover:bg-blue-700" id="btn_proceed_email">Login with code</button>
+                                <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-md text-lg font-medium hover:bg-blue-700" id="btn_proceed_ed">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <ExportData />
+        </div>
+    )
+}
+
+export default Historical
---
+++ src/components/export-data.tsx
@@ -0,0 +1,100 @@
+'use client'
+
+import { useState } from "react";
+import { Info } from "lucide-react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+export default function ExportData() {
+    const [startDate, setStartDate] = useState("2025-03-14");
+    const [endDate, setEndDate] = useState("2025-03-17");
+
+    return (
+        <section className="">
+            {/* Main Content */}
+            <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg">
+                <h2 className="text-2xl font-semibold text-blue-700">Export Data as CSV</h2>
+
+                {/* Warning Box */}
+                <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mt-4 flex gap-2">
+                    <Info className="text-xl" />
+                    <p>
+                        <strong>All data are uncorrected values</strong>
+                        <br />
+                        All air quality parameters exported are raw values. We recommend applying
+                        correction algorithms. See more at{" "}
+                        <a href="#" className="text-blue-600 underline">
+                            Correction Algorithms
+                        </a>
+                        .
+                    </p>
+                </div>
+
+                {/* Filters */}
+                <div className="mt-6 flex flex-wrap gap-4">
+                    <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Select a location" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Locations</SelectLabel>
+                                <SelectItem value="apple">KINAQ Kimwenza</SelectItem>
+                                <SelectItem value="banana">Banana</SelectItem>
+                                <SelectItem value="blueberry">Blueberry</SelectItem>
+                                <SelectItem value="grapes">Grapes</SelectItem>
+                                <SelectItem value="pineapple">Pineapple</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+
+                    <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Select data" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectItem value="apple">Raw Data</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+
+                {/* Date Inputs */}
+                <div className="mt-4 flex flex-wrap gap-4">
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">Start date</label>
+                        <input
+                            type="date"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            className="border p-2 rounded-md w-full focus:border-blue-500 focus:ring-blue-500"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">End date</label>
+                        <input
+                            type="date"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                            className="border p-2 rounded-md w-full"
+                        />
+                    </div>
+                </div>
+
+                {/* Download Button */}
+                <button className="mt-6 bg-orange-500 text-white py-2 px-6 rounded-md text-lg font-semibold hover:bg-orange-600">
+                    Download
+                </button>
+            </div>
+        </section>
+    );
+}
