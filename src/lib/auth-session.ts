ea9e01d557dc57cda207c36a7ab8b195a5fc37af
Victor Kadiata <vickadiata@gmail.com>
2025-04-08 11:28:00 +0100

    feat: Add user session management functions for authentication

---
+++ src/lib/auth-session.ts
@@ -0,0 +1,18 @@
+import { headers } from "next/headers";
+import { auth } from "./auth";
+import { unauthorized } from "next/navigation";
+
+export const getUser = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    return session?.user;
+};
+
+export const getRequiredUser = async () => {
+    const user = await getUser();
+    if (!user) {
+        unauthorized();
+    }
+    return user;
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -66,5 +66,6 @@ export const auth = betterAuth({
                 }
             }
         }),
-        nextCookies()],
+        nextCookies()
+    ],
 });
